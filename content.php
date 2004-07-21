fab527457d7b69936ceab9ec4265fa0306f9538f
jdorner <jdorner@29afd8b9-02df-0310-8cfa-d78a255e3ad3>
2004-07-21 23:39:23 +0000

    html cleanup

--- content.php
+++ content.php
@@ -11,14 +11,7 @@ function getContent($id)
 {
   $files = array('main.php', 'tests.php', 'install.php');
 
-  ob_start();
-  include_once 'Amber/install/header.inc';
   include_once 'Amber/install/' . $files[$id];
-  include_once 'Amber/install/footer.inc';
-  $contents = ob_get_contents();
-  ob_end_clean();
-
-  return $contents;
 }
 
 ?>
--- index.php
+++ index.php
@@ -2,27 +2,33 @@
   require_once 'Amber/install/header.inc';
 ?>
 
-<h1 align="center">Amber</h1>
-
-<p>&nbsp;
-
-<p align="center">
-  <table align="center">
-    <tr>
-      <td width="150" align="center">
-        <div id="0" onclick="showContent(this.id);">Amber</a></div>
-      </td>
-      <td width="150" align="center">
-        <div id="1" onclick="showContent(this.id);">Amber - Tests</div>
-      </td>
-      <td width="150" align="center">
-        <div id="2" onclick="showContent(this.id);">Amber Install Tool</div>
-      </td>
-    </tr>
-  </table>
-</p>
-
-<iframe id="content" width="100%" height="100%"><!-- CONTENT --></div>
+<table height="100%" width="100%">
+  <tr height="50">
+    <td>
+      <h1 align="center">Amber</h1>
+    </td>
+  </tr>
+  <tr height="10">
+    <td>
+      <table align="center">
+        <tr>
+          <td width="150" align="center">
+            <div id="0" onclick="showContent(this.id);">Amber</a></div>
+          </td>
+          <td width="150" align="center">
+            <div id="1" onclick="showContent(this.id);">Amber - Tests</div>
+          </td>
+          <td width="150" align="center">
+            <div id="2" onclick="showContent(this.id);">Amber Install Tool</div>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+  <tr>
+    <td><iframe id="content" src="content.php?id=0" width="100%" height="100%"><!-- CONTENT --></div></td>
+  </tr>
+</table>
 
 <?php
   require_once 'Amber/install/footer.inc';
