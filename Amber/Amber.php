fb5a3cb9be2f4c8d0b90268453f17f851e1677ac
jdorner <jdorner@29afd8b9-02df-0310-8cfa-d78a255e3ad3>
2004-08-03 14:31:23 +0000

    - fixed db access - OpenReport() accepts parameter to reset margin - dump() as new static method

--- Amber/Amber.php
+++ Amber/Amber.php
@@ -1,10 +1,11 @@
 <?php
 
-require_once 'Report.php';
+define('__AMBER_BASE__', dirname(__FILE__));
+
+require_once __AMBER_BASE__ . '/Report.php';
 
 define('AC_DESIGN', 1);
 define('AC_NORMAL', 2);
-define('__AMBER_BASE__', dirname(__FILE__));
 
 class Amber
 {
@@ -13,20 +14,26 @@ class Amber
   /**
    * @return Amber reference to singleton
    */
-  function &getInstance($config=null) {
+  function &getInstance($config = null)
+  {
     static $instance = null;
 
     if (is_null($instance)) {
       $instance = new Amber();
       $instance->_config = $config;
     }
+    
     return $instance;
   }
 
-  function OpenReport($reportName, $mode = AC_NORMAL, $filter = '', $type = 'html')
+  function OpenReport($reportName, $mode = AC_NORMAL, $filter = '', $type = 'html', $noMargin = false)
   {
     $rep =& $this->Loadreport($reportName);
+
     $rep->Filter = $filter;
+    if ($noMargin == true) {
+      $rep->resetMargin();
+    }
 
     // Run it
     switch ($mode) {
@@ -44,7 +51,7 @@ class Amber
   {
     $rep =& new Report();
     $rep->setConfig($this->_config);
-    if ($this->_config->medium == 'db') {
+    if ($this->_config->sys_objects['medium'] == 'db') {
       $rep->setLoader('db');
     } else {
       $rep->setReportDir('reports');
@@ -54,6 +61,11 @@ class Amber
     }
     $rep->load($reportName);
     return $rep;
+  }
+  
+  function dump($var)
+  {
+    echo '<div align="center"><pre style="text-align: left; width: 80%; border: solid 1px #ff0000; font-size: 9pt; color: #000000; background-color: #ffffff; padding: 5px;">' . htmlentities(print_r($var, 1)) . '</pre></div>';
   }
 }
 
