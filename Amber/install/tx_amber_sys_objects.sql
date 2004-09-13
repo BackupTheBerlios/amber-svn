162d0e29990e7a253b678d7c092f318ff5c7ecc3
jdorner <jdorner@29afd8b9-02df-0310-8cfa-d78a255e3ad3>
2004-09-13 07:20:05 +0000

    - updated table structure for tx_amber_sys_objects

--- Amber/install/tx_amber_sys_objects.sql
+++ Amber/install/tx_amber_sys_objects.sql
@@ -1,30 +1,22 @@
--- MySQL-Front 3.0
-
-
--- Host: server5-DEV    Database: typo3_dev
--- ------------------------------------------------------
--- Server version 4.0.18
-
---
--- Table structure for table tx_amber_sys_objects
---
-
-CREATE TABLE `tx_amber_sys_objects` (
-  `uid` int(11) unsigned NOT NULL auto_increment,
-  `pid` int(11) unsigned NOT NULL default '0',
-  `tstamp` int(11) unsigned NOT NULL default '0',
-  `crdate` int(11) unsigned NOT NULL default '0',
-  `cruser_id` int(11) unsigned NOT NULL default '0',
-  `sorting` int(10) unsigned NOT NULL default '0',
-  `deleted` tinyint(4) unsigned NOT NULL default '0',
-  `hidden` tinyint(4) unsigned NOT NULL default '0',
-  `fe_group` int(11) NOT NULL default '0',
-  `name` varchar(255) NOT NULL default '',
-  `design` mediumtext,
-  `class` varchar(255) default NULL,
-  `code` mediumtext,
-  `type` tinyint(4) default NULL,
-  `version` int(4) default NULL,
-  PRIMARY KEY  (`uid`),
-  KEY `parent` (`pid`)
-) TYPE=MyISAM;
+#
+# Table structure for table 'tx_amber_sys_objects'
+#
+CREATE TABLE tx_amber_sys_objects (
+  uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+  pid int(11) unsigned DEFAULT '0' NOT NULL,
+  tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+  crdate int(11) unsigned DEFAULT '0' NOT NULL,
+  cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+  sorting int(10) unsigned DEFAULT '0' NOT NULL,
+  deleted tinyint(3) unsigned DEFAULT '0' NOT NULL,
+  hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+  fe_group int(11) DEFAULT '0' NOT NULL,
+  name varchar(255) DEFAULT '' NOT NULL,
+  design mediumtext,
+  class varchar(255) DEFAULT '',
+  code mediumtext,
+  type tinyint(4) unsigned DEFAULT '0' NOT NULL,
+  version int(11) unsigned DEFAULT '0' NOT NULL,
+  PRIMARY KEY (uid),
+  KEY parent (pid)
+);
