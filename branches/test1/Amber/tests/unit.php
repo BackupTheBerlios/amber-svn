704b47c4a89b8db692380c984d8c09390265b080
jdorner <jdorner@29afd8b9-02df-0310-8cfa-d78a255e3ad3>
2004-11-09 14:49:28 +0000

    deleted test branch

--- branches/test1/Amber/tests/unit.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-#require_once 'PHPUnit.php';
-
-require_once 'PHPUnit.php'
-
-?>
--- branches/test1/examples/reports/Bericht2-Code.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Class Bericht2 extends AmberReport_UserFunctions
-{
-
-  var $interfaceVersion = 2; //Do not change this
-
-}
-?>
