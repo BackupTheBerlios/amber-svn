e110759e944d5d16105d1229f2de66af57ac3968
misetzer <misetzer@29afd8b9-02df-0310-8cfa-d78a255e3ad3>
2005-01-31 21:05:55 +0000

    - added new aggregates var, stdev - added tests for Aggregate functions

--- trunk/Amber/Aggregate.php
+++ trunk/Amber/Aggregate.php
@@ -8,14 +8,16 @@
 */
 require_once 'AggregateFactory.php';
 
-AggregateFactory::register('sum', 'AggregateSum');
-AggregateFactory::register('min', 'AggregateMin');
-AggregateFactory::register('max', 'AggregateMax');
+AggregateFactory::register('sum',   'AggregateSum');
+AggregateFactory::register('min',   'AggregateMin');
+AggregateFactory::register('max',   'AggregateMax');
 AggregateFactory::register('count', 'AggregateCount');
-AggregateFactory::register('avg', 'AggregateAvg');
+AggregateFactory::register('avg',   'AggregateAvg');
 AggregateFactory::register('first', 'AggregateFirst');
-AggregateFactory::register('last', 'AggregateLast');
-AggregateFactory::register('null', 'Aggregate');
+AggregateFactory::register('last',  'AggregateLast');
+AggregateFactory::register('var',   'AggregateVar');
+AggregateFactory::register('stdev', 'AggregateStdev');
+AggregateFactory::register('null',  'Aggregate');
 
 
 
@@ -70,7 +72,10 @@ class AggregateSum  extends Aggregate
    */
   function addValue($value)
   {
-    $this->Value += $value;
+    if ($value === '' || $value === null) {
+    } else {
+      $this->Value += $value;
+    }  
     return $this->Value;
   }
   
@@ -80,7 +85,7 @@ class AggregateSum  extends Aggregate
    */
   function reset()
   {
-    $this->Value = 0;
+    $this->Value = null;
   }
 }
 
@@ -239,3 +244,71 @@ class AggregateLast extends Aggregate   // not much use....
     $this->Value = null;
   }
 }  
+
+class AggregateVar extends Aggregate
+{
+  /**
+   * @access public
+   * @param anyType
+   */
+  function addValue($value)
+  {
+    if ($value === '' || $value === null) {
+    } else {
+      $this->_cnt ++;
+      $this->_sum += $value;
+      $this->_sum2 += $value * $value;
+      if ($this->_cnt > 1) {
+        $varN = (($this->_sum2 / $this->_cnt) - pow($this->_sum / $this->_cnt, 2)); 
+        $this->Value = $varN / ($this->_cnt - 1) * $this->_cnt;
+      }  
+    }  
+    return $this->Value;
+  }
+  
+  /**
+   * @access public
+   * @param anyType
+   */
+  function reset()
+  {
+    $this->_cnt = 0;
+    $this->_sum = 0;
+    $this->_sum2 = 0;
+    $this->Value = null;
+  }
+}
+
+class AggregateStdev extends Aggregate
+{
+  /**
+   * @access public
+   * @param anyType
+   */
+  function addValue($value)
+  {
+    if ($value === '' || $value === null) {
+    } else {
+      $this->_cnt ++;
+      $this->_sum += $value;
+      $this->_sum2 += $value * $value;
+      if ($this->_cnt > 1) {
+        $varN = (($this->_sum2 / $this->_cnt) - pow($this->_sum / $this->_cnt, 2)); 
+        $this->Value = sqrt($varN / ($this->_cnt - 1) * $this->_cnt);
+      }
+    }  
+    return $this->Value;
+  }
+  
+  /**
+   * @access public
+   * @param anyType
+   */
+  function reset()
+  {
+    $this->_cnt = 0;
+    $this->_sum = 0;
+    $this->_sum2 = 0;
+    $this->Value = null;
+  }
+}
---
+++ trunk/Amber/tests/testAggregate.php
@@ -0,0 +1,237 @@
+<?php
+
+/**
+*
+* @package Amber
+* @subpackage Tests
+*
+*/
+ini_set('include_path', ini_get('include_path') . ':' . dirname(__FILE__). '/../../Amber/');
+
+require_once 'unit.php';
+require_once '../AggregateFactory.php';
+require_once '../Aggregate.php';
+
+/**
+*
+* @package Amber
+* @subpackage Tests
+*
+*/
+#class myTestCase extends PHPUnit_TestCase
+#{
+#  function assertEEquals($expected, $given, $message='')
+#  {
+#    parent::assertEquals($expected, $given, $message . "  expected:" . $expected . "; given:" . $given . "; ");
+#  }
+#}
+
+/**
+*
+* @package Amber
+* @subpackage Tests
+*
+*/
+class Aggregate_Basic extends PHPUnit_TestCase
+{
+  function test()
+  {
+    $agg = AggregateFactory::create('sum');
+    $this->assertNull($agg->Value,       'test Sum 0');
+    
+    $list = array(1, 2, 3, 4, -4, '2', '', null);
+    $agg->reset();
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(8, $agg->Value,       'test Sum 1');
+
+    $list = array('', null);
+    $agg->reset();
+    $this->assertNull($agg->Value,       'test Sum 2');
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertNull($agg->Value,       'test Sum 3');
+  
+  
+//  function test_Count()
+  
+    $agg = AggregateFactory::create('count');
+    $this->assertEquals(0, $agg->Value,       'test count 0');
+    
+    $list = array(1, 2, 3, 4, -4, '2', '', null);
+    $agg->reset();
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(6, $agg->Value,       'test count 1');
+
+    $list = array('', null);
+    $agg->reset();
+    $this->assertEquals(0, $agg->Value,       'test count 2');
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(0, $agg->Value,       'test count 3');
+  
+  
+//  function test_Avg()
+  
+    $agg = AggregateFactory::create('avg');
+    $this->assertEquals(null, $agg->Value,       'test avg 0');
+    
+    $list = array(1, 2, 3, 4, -4, '2', '', null);
+    $agg->reset();
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(4, 3*$agg->Value,     'test avg 1');
+
+    $list = array('', null);
+    $agg->reset();
+    $this->assertNull($agg->Value,       'test avg 2');
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(null, $agg->Value,       'test avg 3');
+  
+  
+//  function test_Min()
+  
+    $agg = AggregateFactory::create('min');
+    $this->assertEquals(null, $agg->Value, 'test min 0');
+    
+    $agg->reset();
+    $list = array(1, 2, 3, 4, -4, '2', '', null);
+    $agg->reset();
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(-4, $agg->Value,   'test min 1');
+
+    $list = array(1, 2, 3, 4, '2', '', null);
+    $agg->reset();
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(1, $agg->Value,    'test min 1a');
+
+    $list = array('', null);
+    $agg->reset();
+    $this->assertNull($agg->Value,         'test min 2');
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(null, $agg->Value, 'test min 3');
+
+
+//  function test_Max()
+  
+    $agg = AggregateFactory::create('max');
+    $this->assertEquals(null, $agg->Value, 'test max 0');
+    
+    $agg->reset();
+    $list = array(1, 2, 3, 4, -4, '2', '', null);
+    $agg->reset();
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(4, $agg->Value,    'test max 1');
+
+    $list = array(-1, -2, -3, -4, '-2', '', null);
+    $agg->reset();
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(-1, $agg->Value,   'test max 1a');
+
+    $list = array('', null);
+    $agg->reset();
+    $this->assertNull($agg->Value,         'test max 2');
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(null, $agg->Value, 'test max 3');
+  
+ 
+//  function test_First()
+  
+    $agg = AggregateFactory::create('first');
+    $this->assertEquals(null, $agg->Value, 'test first 0');
+    
+    $agg->reset();
+    $list = array(1, 2, 3, 4, -4, '2', '', null);
+    $agg->reset();
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(1, $agg->Value,    'test first 1');
+
+    $list = array(null, -1, -2, -3, -4, '-2', '', null);
+    $agg->reset();
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertNull($agg->Value,         'test first 1a');
+
+    $list = array('', null);
+    $agg->reset();
+    $this->assertNull($agg->Value,         'test first 2');
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals('', $agg->Value,   'test first 3');
+  
+
+//  function test_Last()
+  
+    $agg = AggregateFactory::create('last');
+    $this->assertEquals(null, $agg->Value, 'test last 0');
+    
+    $agg->reset();
+    $list = array(1, 2, 3, 4, -4, '2', '', null, 1);
+    $agg->reset();
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(1, $agg->Value,    'test last 1');
+
+    $list = array(null, -1, -2, -3, -4, '-2', '', null);
+    $agg->reset();
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertNull($agg->Value,         'test last 1a');
+
+    $list = array(null, '');
+    $agg->reset();
+    $this->assertNull($agg->Value,         'test last 2');
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals('', $agg->Value,   'test last 3');
+  
+
+//  function test_Var()
+  
+    $agg = AggregateFactory::create('var');
+    $this->assertEquals(null, $agg->Value,  'test var 0');
+    
+    $agg->reset();
+    $list = array(9, 1, 2, '', null);
+    $agg->reset();
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(19, $agg->Value,     'test var 1');
+
+    $list = array(9);
+    $agg->reset();
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertNull($agg->Value,           'test var 1a');
+
+    $list = array(null, '');
+    $agg->reset();
+    $this->assertNull($agg->Value,           'test var 2');
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(null, $agg->Value,   'test var 3');
+  
+
+//  function test_Stdev()
+  
+    $agg = AggregateFactory::create('stdev');
+    $this->assertEquals(null, $agg->Value,   'test stdev 0');
+    
+    $agg->reset();
+    $list = array(9, 1, 2, '', null);
+    $agg->reset();
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(sqrt(19), $agg->Value,'test stdev 1');
+
+    $list = array(9);
+    $agg->reset();
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertNull($agg->Value,           'test stdev 1a');
+
+    $list = array(null, '');
+    $agg->reset();
+    $this->assertNull($agg->Value,           'test stdev 2');
+    foreach ($list as $item) { $agg->addvalue($item); }
+    $this->assertEquals(null, $agg->Value,   'test stdev 3');
+  }
+}
+
+/**
+*
+* @package Amber
+* @subpackage Tests
+*
+*/
+
+$suite  = new PHPUnit_TestSuite("Aggregate_Basic");
+$result = PHPUnit::run($suite);
+echo $result -> toHTML();
+?>
