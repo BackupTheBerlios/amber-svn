4e023b103334a4092151c0a4a5f0758d199a15e0
jdorner <jdorner@29afd8b9-02df-0310-8cfa-d78a255e3ad3>
2005-12-30 08:31:22 +0000

    reworked HTML-Frontend: - fixed Javascript-bug - fixed link "Run all tests" - modified layout and font color

--- trunk/index.php
+++ trunk/index.php
@@ -2,33 +2,19 @@
   require_once 'Amber/install/header.inc';
 ?>
 
-<table height="100%" width="100%">
-  <tr height="50">
-    <td>
-      <h1 align="center">Amber</h1>
-    </td>
-  </tr>
-  <tr height="10">
-    <td>
-      <table align="center">
-        <tr>
-          <td width="150" align="center">
-            <div id="0" onclick="showContent(this.id);">Amber</a></div>
-          </td>
-          <td width="150" align="center">
-            <div id="1" onclick="showContent(this.id);">Amber - Tests</div>
-          </td>
-          <td width="150" align="center">
-            <div id="2" onclick="showContent(this.id);">Amber Install Tool</div>
-          </td>
-        </tr>
-      </table>
-    </td>
-  </tr>
-  <tr>
-    <td><iframe id="content" src="content.php?id=0" width="100%" height="100%" frameborder="0"><!-- CONTENT --></div></td>
-  </tr>
-</table>
+<h1 align="center">Amber</h1>
+
+<hr />
+
+<ul id="menu">
+  <li id="0" onclick="showContent(this.id);">Amber - Main</li>
+  <li id="1" onclick="showContent(this.id);">Amber - Tests</li>
+  <li id="2" onclick="showContent(this.id);">Amber - Install Tool</li>
+</ul>
+
+<hr />
+
+<iframe id="content2" src="content.php?id=0" width="100%" height="100%" frameborder="0"><!-- CONTENT --></div>
 
 <?php
   require_once 'Amber/install/footer.inc';
